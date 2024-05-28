2f8500b30d476e0135818cd956a76f11794b9d7a
Sergio Barreras <sergiobg.isc@gmail.com>
2024-05-28 11:24:48 -0700

    notifier model update

--- lib/src/models/audioplayer_model.dart
+++ lib/src/models/audioplayer_model.dart
@@ -1,12 +1,46 @@
 import 'package:flutter/material.dart';
 
 class AudioPlayerModel with ChangeNotifier {
+  late AnimationController controller;
+  Duration _songDuration = const Duration(microseconds: 0);
+  Duration _current = const Duration(microseconds: 0);
   bool _playing = false;
-  late AnimationController _controller;
 
-  set controller(AnimationController value) {
-    _controller = value;
+  bool get playing => _playing;
+  Duration get songDuration => _songDuration;
+  Duration get current => _current;
+
+  double get percentage => (_songDuration.inSeconds > 0)
+      ? (_current.inSeconds / _songDuration.inSeconds)
+      : 0;
+
+  String get songTotalDuration => printDuration(_songDuration);
+  String get currentSecond => printDuration(_current);
+
+  set playing(bool value) {
+    _playing = value;
+    notifyListeners();
   }
 
-  AnimationController get controller => _controller;
+  set songDuration(Duration value) {
+    _songDuration = value;
+    notifyListeners();
+  }
+
+  set current(Duration value) {
+    _current = value;
+    notifyListeners();
+  }
+
+  String printDuration(Duration duration) {
+    String twoDigits(int n) {
+      if (n >= 10) return "$n";
+      return "0$n";
+    }
+
+    String twoDigitMins = twoDigits(duration.inMinutes.remainder(60));
+    String twoDigitSecs = twoDigits(duration.inSeconds.remainder(60));
+
+    return "$twoDigitMins:$twoDigitSecs";
+  }
 }
