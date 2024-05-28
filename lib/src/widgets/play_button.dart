e1e83f2931fafcf9fb0ffebfc779dca492d4b0e7
Sergio Barreras <sergiobg.isc@gmail.com>
2024-05-28 10:19:44 -0700

    animated icon

--- lib/src/widgets/play_button.dart
+++ lib/src/widgets/play_button.dart
@@ -1,7 +1,29 @@
 import 'package:flutter/material.dart';
 
-class PlayButton extends StatelessWidget {
+class PlayButton extends StatefulWidget {
   const PlayButton({super.key});
+
+  @override
+  State<PlayButton> createState() => _PlayButtonState();
+}
+
+class _PlayButtonState extends State<PlayButton>
+    with SingleTickerProviderStateMixin {
+  bool isPlaying = false;
+  late AnimationController playAnimation;
+
+  @override
+  void initState() {
+    playAnimation = AnimationController(
+        vsync: this, duration: const Duration(milliseconds: 500));
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    playAnimation.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -9,9 +31,20 @@ class PlayButton extends StatelessWidget {
       elevation: 0,
       highlightElevation: 0,
       backgroundColor: const Color(0xfff8cb51),
-      onPressed: () {},
+      onPressed: () {
+        if (isPlaying) {
+          playAnimation.reverse();
+          isPlaying = false;
+        } else {
+          playAnimation.forward();
+          isPlaying = true;
+        }
+      },
       shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(100)),
-      child: Icon(Icons.play_arrow, color: Color(0xff1C1C25)),
+      child: AnimatedIcon(
+          progress: playAnimation,
+          icon: AnimatedIcons.play_pause,
+          color: const Color(0xff1C1C25)),
     );
   }
 }
